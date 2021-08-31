d5cc85e2067774379ee18dbed8eb95eb2478ff2c
Code Sphinx <31335736+codesphinxx@users.noreply.github.com>
2021-08-31 09:58:19 -0500

    ...

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export {default as LogClient} from './log.client';
 export {default as DeviceInformer} from './device.informer';
 
 export {default as PlaynixBaseClient} from './playnix.base';
+
+export {default as  Utils} from './utils';
